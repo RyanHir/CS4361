b335b0104d0ea3a0ecba411335aa869beab3e410
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2024-11-13 13:31:25 -0600

    End of Episode 05

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    void OnCollisionEnter(Collision info)
+    {
+        // Ignore non obstical collisions
+        if (info.collider.tag != "Obstical")
+            return;
+        movement.enabled = false;
+    }
+}
