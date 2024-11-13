5a138e694a3edb495771b2a8718a3e7174aad415
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2024-11-13 13:46:41 -0600

    End of Episode 06

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -15,18 +15,18 @@ public class PlayerMovement : MonoBehaviour
         // Applying a forward force to player object
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-        bool keyLeft = Input.GetKey("a"); 
-        bool keyRight = Input.GetKey("d"); 
+        bool keyLeft = Input.GetKey("a");
+        bool keyRight = Input.GetKey("d");
 
         if (keyLeft || keyRight)
         {
             if (keyRight)
             {
-                rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+                rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
             }
             else if (keyLeft)
             {
-                rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+                rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 
             }
         }
