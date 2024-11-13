cf843f351aa66d30098623ae56a5dbf836c4bb77
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2024-11-13 13:13:19 -0600

    End of Episode 03

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private const float FORWARD_FORCE = 500;
+    public float forwardForce = 500;
+    public float sideForce = 500;
     public Rigidbody rb;
 
     void Start()
@@ -12,6 +13,23 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, FORWARD_FORCE * Time.deltaTime);
+        // Applying a forward force to player object
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        const bool keyLeft = Input.GetKey("a"); 
+        const bool keyRight = Input.GetKey("d"); 
+
+        if (keyLeft || keyRight)
+        {
+            if (keyRight)
+            {
+                rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+            }
+            else if (keyLeft)
+            {
+                rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+
+            }
+        }
     }
 }
